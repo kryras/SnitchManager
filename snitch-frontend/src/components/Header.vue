e952e374ae2cd1c68d01972856bb33eaaf03541e
kryras <v4m4n4@gmail.com>
2021-01-05 22:08:23 +0100

    fix: change header size and z-index

--- snitch-frontend/src/components/Header.vue
+++ snitch-frontend/src/components/Header.vue
@@ -41,20 +41,24 @@ export default {
 .background {
   position: absolute;
   overflow: hidden;
-  z-index: -1;
+  z-index: -100;
   left: 0;
   width: 100vw;
-  height: 600px;
+  height: 670px;
   border-radius: 0 0 50% 50%/0 0 100% 100%;
   background: linear-gradient(130deg, #462f88 18.56%, #4baec4 85.45%);
   transform: scaleX(3);
   transition: transform 0.2s ease-in-out;
   @media (max-width: $mobile-max-width) {
     transform: scaleX(4);
+    height: 610px;
   }
 }
 
 .nav-background {
-  transform: translate(0, -270px) scale(2.8, 0.1);
+  transform: translate(0, -302px) scale(2.8, 0.1);
+  @media (max-width: $mobile-max-width) {
+    transform: translate(0, -275px) scale(2.8, 0.1);
+  }
 }
 </style>
