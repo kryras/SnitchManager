c5e592071cb6683836783a23358b04139088a3c4
kryras <v4m4n4@gmail.com>
2021-02-07 22:20:14 +0100

    feat: add interceptor to request with auth header

--- snitch-frontend/src/services/repository.js
+++ snitch-frontend/src/services/repository.js
@@ -1,7 +1,22 @@
 import axios from "axios";
+import authHeader from "./auth-header"
 
 const baseURL = `${process.env.VUE_APP_BASE_URL}/api`;
 
-export default axios.create({
-    baseURL
+const instance = axios.create({
+    baseURL,
 })
+
+instance.interceptors.request.use(
+    async config => {
+        const authToken = await authHeader()
+        config.headers = {
+            'Authorization': `${authToken.Authorization}`
+        }
+        return config;
+    },
+    error => {
+        Promise.reject(error)
+    });
+
+export default instance;
