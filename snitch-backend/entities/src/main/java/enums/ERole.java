ac1d9b158f861b2d6e3ddeda7175a58ef378490e
Forestf <michalsliwa96@wp.pl>
2021-02-09 17:16:19 +0100

    Add role name to admin panel

--- snitch-backend/entities/src/main/java/enums/ERole.java
+++ snitch-backend/entities/src/main/java/enums/ERole.java
@@ -3,5 +3,18 @@ package enums;
 public enum ERole {
     ROLE_USER,
     ROLE_MODERATOR,
-    ROLE_ADMIN
+    ROLE_ADMIN;
+    @Override
+    public String toString(){
+        switch(this) {
+            case ROLE_USER:
+                return "User";
+            case ROLE_MODERATOR:
+                return "Moderator";
+            case ROLE_ADMIN:
+                return "Admin";
+            default:
+                return "";
+        }
+    }
 }
